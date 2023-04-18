ab89ca45a7cffeb6399070172b8fdfa8c500b45b
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2023-04-18 20:03:27 +0400

    Player state and player transition templates

---
+++ Assets/Scripts/Player/States/PlayerState.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerState : MonoBehaviour
+{
+    [SerializeField] private PlayerTransition[] _transitions;
+
+    protected Rigidbody Rigidbody { get; private set; }
+    protected Animator Animator { get; private set; }
+
+    public void Enter(Rigidbody rigidbody, Animator animator)
+    {
+        if (enabled == false)
+        {
+            Rigidbody = rigidbody;
+            Animator = animator;
+
+            enabled = true;
+
+            foreach (var transition in _transitions)
+            {
+                transition.enabled = true;
+            }
+        }
+
+    }
+
+
+
+    public void Exit()
+    {
+        if (enabled == true)
+        {
+            foreach (var transition in _transitions)
+            {
+                transition.enabled = false;
+            }
+
+            enabled = false;
+        }
+
+    }
+
+
+    public PlayerState GetNextState()
+    {
+        foreach (var transition in _transitions)
+        {
+            if (transition.NeedTransit)
+            {
+                return transition.TargetState;
+            }
+        }
+
+        return null;
+    }
+
+}
