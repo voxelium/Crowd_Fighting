21966f7969c318b4f063b194432c703437260ce7
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2023-04-25 12:25:36 +0400

    Enemy attack Root Rotate fixed

--- Assets/Scripts/Enemy/Enemy_Man/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/Enemy_Man/EnemyAttackState.cs
@@ -8,13 +8,18 @@ public class EnemyAttackState : StateMachineBehaviour
 
     Transform player;
     NavMeshAgent agent;
-    private float stopAttackRange = 2;
+    private float stopAttackRange = 1.5f;
+    private EnemyRoot enemyRoot;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = animator.GetComponent<NavMeshAgent>();
+
+        enemyRoot = agent.GetComponentInChildren<EnemyRoot>();
+        enemyRoot.transform.Rotate(0, 60, 0, Space.Self);
+
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -34,7 +39,7 @@ public class EnemyAttackState : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        enemyRoot.transform.Rotate(0,-60,0, Space.Self);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
